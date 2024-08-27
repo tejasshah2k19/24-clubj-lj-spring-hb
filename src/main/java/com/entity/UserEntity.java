361a1024f2da540aedb4110fa3b67ece4d92d90c
Tejas Shah <tejasshah2k19@gmail.com>
2024-08-27 16:59:24 +0530

    spring boot + hb + init

---
+++ src/main/java/com/entity/UserEntity.java
@@ -0,0 +1,96 @@
+package com.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+
+@Entity // this will notify hb to create table in database
+//what is the name of table?->default its entity name 
+//but we can explicitly assign name using @Table 
+@Table(name = "users")
+public class UserEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Integer userId;
+
+	@Column(length = 25)
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	private String role;// [ STUDENT,ADMIN,CLERK,XXX]
+	// lombok we will do
+	@Transient
+	private String otp; 
+	private boolean active; 
+	
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getOtp() {
+		return otp;
+	}
+
+	public void setOtp(String otp) {
+		this.otp = otp;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+}
